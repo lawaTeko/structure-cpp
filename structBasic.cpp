3c1129ca1c2edeb3b500ee152853d2414098b294
lawateko <123440000+lawaTeko@users.noreply.github.com>
2025-01-03 05:42:13 +0700

    Update structBasic.cpp
    menambahkan 1 struct pada baris kode

--- structBasic.cpp
+++ structBasic.cpp
@@ -10,23 +10,42 @@ struct makanan{  // deklarasi struct
 	string rasa_makanan; // member (string)
 };
 
+struct minuman{
+	string nama_minuman;
+	string asal_minuman; 
+	string rasa_minuman; 
+};
+
 
 int main(){
 	
-	// membuat struct sederhana : Makanan Daerah
-	
+	// membuat struct sederhana : Makanan Daerah dan Minuman
 	makanan lumpia;
+	minuman esCendol;
 	
 	// struct makanan 
 	lumpia.nama_makanan = "Lumpia Sedap";
 	lumpia.asal_makanan = "Jawa Tengah";
 	lumpia.rasa_makanan = "Rasa garing di luar dan lembut di dalam! Mantap";
+
+	// struct minuman 
+	esCendol.nama_minuman = "Es Cendol";
+	esCendol.asal_minuman = "Jawa Barat";
+	esCendol.rasa_minuman = "Manis dan Menyegarkan";
+
 	
 	// output
 	cout << "Makanan Daerah 1 : " << endl;
 	cout << lumpia.nama_makanan << endl;
 	cout << lumpia.asal_makanan << endl;
 	cout << lumpia.rasa_makanan << endl;
+
+	cout << endl;
+	
+	cout << "Minuman Daerah 2 : " << endl;
+	cout << esCendol.nama_minuman << endl;
+	cout << esCendol.asal_minuman << endl;
+	cout << esCendol.rasa_minuman << endl;
 	
 	return 0;
 }
